659eb61e1d519f181e2dba0f1c9162255f5cc47c
mohamedsat aroussi <159490099+Mohamed-aroussi@users.noreply.github.com>
2024-02-11 22:50:17 +0100

    Add files via upload

---
+++ tutral.js
@@ -0,0 +1,28 @@
+
+
+
+const mohamed1 = document.getElementById("tt")
+
+//mohamed1.style.color = "red"
+
+//mohamed1.style.backgroundColor = "red"
+
+
+
+const name = (params) => {
+    
+}
+
+
+
+
+const mohamed = document.querySelector(".my")
+
+btn-pack2.addEventListener( "click" ,  (params) => {
+    mohamed1.style.color = "red"
+})
+
+
+buy.addEventListener( "click" ,  (params) => {
+    buy.classList.add("css")
+})
